72403b44c741d6bb1fe20737a90ce74d489e91ea
Elakkiya.Elangoraja <Elakkiya.Elangoraja@AGLS-LT-207>
2022-07-30 11:30:24 +0530

    Add project files.

---
+++ MyWalletTracker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyWalletTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
